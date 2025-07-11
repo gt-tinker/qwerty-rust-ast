a5763130327c818b55c87b411eb3e5c39f6dccff
Austin Adams <git@austinjadams.com>
2025-07-11 12:23:27 -0400

    Use conventional extension module naming (_qwerty)

--- qwerty_pyrt/rust/src/lib.rs
+++ qwerty_pyrt/rust/src/lib.rs
@@ -8,7 +8,8 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn qwerty(m: &Bound<'_, PyModule>) -> PyResult<()> {
+#[pyo3(name = "_qwerty_pyrt")]
+fn qwerty_pyrt(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
